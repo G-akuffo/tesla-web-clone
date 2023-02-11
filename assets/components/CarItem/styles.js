9d2651901aa103c4858438d4a2122d762ee31ec0
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2023-02-11 16:34:14 +0000

    CarItem component

---
+++ assets/components/CarItem/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    alignItems: "center",
+    width: "100&",
+    marginTop: "30%",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 500,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#5c5e62",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover",
+    position: "absolute",
+  },
+});
+
+export default styles;
