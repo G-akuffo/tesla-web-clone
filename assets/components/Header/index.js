cd3a282706d609c684bc38705559fe276b9b5798
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2023-02-12 17:06:48 +0000

    ui done

--- assets/components/Header/index.js
+++ assets/components/Header/index.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import styles from "./styles";
 
 const Header = () => {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("../../images/logo.png")} />
-      <Image style={styles.menu} source={require("../../images/menu.png")} />
+      <TouchableOpacity>
+        <Image style={styles.menu} source={require("../../images/menu.png")} />
+      </TouchableOpacity>
     </View>
   );
 };
